846ae370679a08e43c9f513652d337b0508da91c
SAMSUNG <SAMSUNG@LAPTOP-8E9GRRV8>
2020-03-19 18:43:49 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println ("Book Management System Menu");
+		System.out.println ("1. Add Book");
+		System.out.println ("2. Delete Book");
+		System.out.println ("3. Edit Book");
+		System.out.println ("4. View Book");
+		System.out.println ("5. Exit");
+		System.out.println ("Select one number between 1 - 5 :");
+	}
+
+}
