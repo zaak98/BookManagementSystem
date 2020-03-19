4aeddc757dcc7e651654019324e4dcfb79d32e4f
zaak98 <62365133+zaak98@users.noreply.github.com>
2020-03-19 17:27:28 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println ("Hello, world");
+		System.out.println ("I am a new Java prgrammer");
+	}
+
+}
